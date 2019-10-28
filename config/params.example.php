32091e401e9cd15867875ea4c09cc4962fbc9002
Maxim Melnik <mm@albedo.dev>
2019-10-28 17:19:15 +0200

    Default proxies link changed

--- config/params.example.php
+++ config/params.example.php
@@ -5,5 +5,5 @@ return [
     'dbname' => 'scraper',
     'user' => 'root',
     'password' => '',
-    'proxiesLink' => 'http://api.best-proxies.ru/proxylist.json?key=ba94f47b89e7ce92facb36a36775e433'
+    'proxiesLink' => 'https://api.best-proxies.ru/proxylist.json?key=ba94f47b89e7ce92facb36a36775e433&type=https'
 ];

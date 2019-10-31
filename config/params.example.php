21ceb8ff5f65344dc019ab2f656669062e619b46
Maxim Melnik <mm@albedo.dev>
2019-10-31 15:32:59 +0200

    Proxy link changed

--- config/params.example.php
+++ config/params.example.php
@@ -5,6 +5,6 @@ return [
     'dbname' => 'scraper',
     'user' => 'root',
     'password' => '',
-    'proxiesLink' => 'https://api.best-proxies.ru/proxylist.json?key=ba94f47b89e7ce92facb36a36775e433&type=https'
+    'proxiesLink' => 'http://api.best-proxies.ru/proxylist.json?key=ba94f47b89e7ce92facb36a36775e433&type=https&level=1&country=de,ru,pl,cz,es,nl,us&speed=1'
     // Link must contain proxies array with port and ip keys
 ];
